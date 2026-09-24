265873d8b0b2092363c1bff5a8657269187f99d5
wajimahisayoshi <simasaba718@gmail.com>
2026-09-24 20:39:58 +0900

    Create Bookjava

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book extends TangibleAssrt{
+  private String isbn;
+ 
+ public Book(String name,int price,String color,String isbn){
+	 super(name,price,color);
+	 this.isbn;
+ }
+
+ public String getlsbn(){ return this.isbn;}
+}
+
